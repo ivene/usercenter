1b63dd11877e63b8c6e6d933bf06c108d03a51de
Yao.Yao <yao_ivene@163.com>
2018-03-13 14:38:27 +0800

    first commit

---
+++ src/UserCenterProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Ivene\UserCenter;
+
+use Illuminate\Support\ServiceProvider;
+
+class UserCenterProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
